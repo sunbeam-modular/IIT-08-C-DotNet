908ae8d3cbce6283d9ae4eb7502feefa36723576
PriyaRangole <priyarangole0218@gmail.com>
2025-12-15 16:54:16 +0530

    Reflection demo added

---
+++ Day4/13DemoReflection/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Reflection;
+
+namespace _13DemoReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\KDAC_Demos\\KDAC\\MathLib\\bin\\Debug\\MathLib.dll";
+            //string path = "D:\\KDAC_Demos\\KDAC\\02DemoOOP_Editor\\bin\\Debug\\net8.0\\02DemoOOP_Editor.dll";
+
+            Assembly assembly = Assembly.LoadFrom(path);
+
+            Type[] allTypesInAssembly = assembly.GetTypes();
+
+            foreach (Type type in allTypesInAssembly)
+            {
+                Console.WriteLine("Is Public : " + type.IsPublic);
+                Console.WriteLine("Is Class : " + type.IsClass);
+                Console.WriteLine("FullName  : " + type.FullName);
+
+                IEnumerable<Attribute> allAttributesOnType =
+                     type.GetCustomAttributes();
+
+                bool IsSerializable = false;
+                foreach (Attribute attribute in allAttributesOnType)
+                {
+                    if (attribute is SerializableAttribute)
+                    {
+                        IsSerializable = true;
+                        break;
+                    }
+                }
+
+                if (IsSerializable)
+                {
+                    Console.WriteLine(type.Name  + " is Serializable!");
+                }
+                else
+                {
+                    //Console.WriteLine(type.Name + " is NOT Serializable!");
+
+                    throw new Exception(type.Name + " is not marked as Serializable");
+                }
+
+
+
+                MethodInfo[] allMethods = type.GetMethods();
+
+                Console.WriteLine("Methods : ");
+                Console.WriteLine();
+                foreach (MethodInfo method in allMethods)
+                {
+                    Console.Write(" -- " + 
+                                       method.ReturnType.ToString() 
+                                        + " " + method.Name + "( ");
+
+                    ParameterInfo[] allParams =
+                        method.GetParameters();
+
+                    foreach (ParameterInfo para in allParams)
+                    {
+                        Console.Write(
+                            para.ParameterType.ToString() + " "
+                            +para.Name + " ");
+                    }
+
+                    Console.Write(" )");
+                    Console.WriteLine();
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day4/14DemoReflection/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection;
+
+namespace _14DemoReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\KDAC_Demos\\KDAC\\MathLib\\bin\\Debug\\MathLib.dll";
+            Assembly assembly = Assembly.LoadFrom(path);
+
+            Type[] allTypesInAssembly = assembly.GetTypes();
+
+            foreach (Type type in allTypesInAssembly)
+            {
+                
+                Console.WriteLine("FullName  : " + type.FullName);
+
+                object dynamicObejct = 
+                    assembly.CreateInstance(type.FullName);
+
+                Console.WriteLine("Object created for type " + type.FullName);
+
+                MethodInfo[] allMethods = type.GetMethods();
+
+                foreach (MethodInfo method in allMethods)
+                {
+                    Console.WriteLine(" -- Calling : " + method.Name );
+
+                    ParameterInfo[] allParams =
+                        method.GetParameters();
+
+                    object[] arguments = new object[allParams.Length];
+
+
+                    for (int i = 0; i < allParams.Length; i++)
+                    {
+                        ParameterInfo para = allParams[i];
+
+                        Console.WriteLine("Enter data of type " + 
+                            para.ParameterType.ToString() + " for "
+                            + para.Name + " ");
+
+                        string data = Console.ReadLine();
+                        arguments[i] = 
+                                Convert.ChangeType(data, para.ParameterType);
+
+
+                    }
+
+                  object output = 
+                        type.InvokeMember(method.Name,
+                        BindingFlags.Public |
+                        BindingFlags.Instance |
+                        BindingFlags.InvokeMethod,
+                        null, dynamicObejct, arguments);
+                    Console.WriteLine(output);
+                    Console.WriteLine("-------------------");
+
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
