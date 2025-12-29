b99a262908a62896f03ce2ebbb2afe445652a626
PriyaRangole <priyarangole0218@gmail.com>
2025-12-29 11:14:43 +0530

    email service data uploded

---
+++ Project/EmailService/SmtpMailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+
+namespace EmailService
+{
+    
+
+    
+
+    public class SmtpEmailSender : IEmailSender
+    {
+        private readonly SmtpSettings _settings;
+
+        public SmtpEmailSender(IOptions<SmtpSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string body)
+        {
+            using var client = new SmtpClient(_settings.Host, _settings.Port)
+            {
+                Credentials = new NetworkCredential(_settings.UserName, _settings.Password),
+                EnableSsl = _settings.EnableSsl
+            };
+
+            var mail = new MailMessage
+            {
+                From = new MailAddress(_settings.FromEmail, _settings.FromName),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true // set to false if you want plain text
+            };
+
+            mail.To.Add(toEmail); // fixed incorrect syntax
+
+            await client.SendMailAsync(mail);
+        }
+    }
+}
